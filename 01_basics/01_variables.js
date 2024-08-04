706d3023a1deeb77c7cf1899a089cac8453ef95e
1522003 <133994899+1522003@users.noreply.github.com>
2024-08-04 11:57:37 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,21 @@
+const accountId = 144553
+let accountEmail = "akash@google.com"
+var accountPassword = "12345"
+accountCity = "bbsr"
+let accountState;
+
+// accountId = 2  //not allowed  {scope}
+
+
+
+accountEmail = "fhe4@gmail.com"
+accountPassword = "112121121"
+accountCity = "odisha"
+
+console.log(accountId);
+/*
+prefer not use var
+because issue in block scope and functional scope
+*/
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState]);
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,28 @@
+"use strict";//treat all js code as newerr version
+
+//alert("hello") // we are using nodejs, not browser
+
+console.log(3+3); 
+
+console.log("akash");
+
+let name = "Akash"
+let age = 21
+let isloggedIn = false
+let state = null
+
+//number => 2 to power 53
+//bigint 
+//string => ""
+//boolean => true/false
+//null =>standalone value
+//underlined => 
+//symbol => find the uniqueness
+
+//object
+console.log(typeof "Akash");
+console.log(typeof age);
+console.log(typeof undefined);
+console.log(typeof null); //object
+
+
